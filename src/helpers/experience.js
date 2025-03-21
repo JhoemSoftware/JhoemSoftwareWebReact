5f4ef20c6c13998940cc931de4cf7d2c221320df
Jhoem Software <www.jhonlive90@gmail.com>
2025-03-21 14:07:09 -0500

    Updates styles

--- src/helpers/experience.js
+++ src/helpers/experience.js
@@ -1,39 +1,29 @@
 export const expeciences = [
     {
-        id: 1,
-        name: 'Conocimiento Competitivo',
-        img: 'experience1',
-        description: 'MERN Full-Stack App where we can search for popular GitHub repositories',
-        url_web: 'https://fullstack-mern-github.onrender.com/',
+        id: 4,
+        name: 'Fenalco Antioquia',
+        img: 'experience4',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur repudiandae voluptates voluptas dolorem expedita esse eum',
+        date_in: 'Feb 2020',
+        date_out: 'Present',
+        url_web: 'https://www.fenalcoantioquia.com/',
         tecnologies: [
-            'android',
-            'java',
-            'c_sharp',
-            'unity',
-            'js',
-            'wordpress'
-        ]
-    },
-    {
-        id: 2,
-        name: 'Tera Click',
-        img: 'experience2',
-        description: 'MERN Full-Stack App where we can search for popular GitHub repositories',
-        url_web: 'https://fullstack-mern-github.onrender.com/',
-        tecnologies: [
-            'html',
-            'css',
             'php',
-            'wordpress',
             'js',
+            'laravel',
+            'docker',
+            'react',
+            'vue'
         ]
     },
     {
         id: 3,
         name: 'OPA Soluciones',
         img: 'experience3',
-        description: 'MERN Full-Stack App where we can search for popular GitHub repositories',
-        url_web: 'https://fullstack-mern-github.onrender.com/',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur repudiandae voluptates voluptas dolorem expedita esse eum',
+        date_in: 'Aug 2019',
+        date_out: 'Feb 2020',
+        url_web: 'https://www.opa.com.co/',
         tecnologies: [
             'c_sharp',
             'html',
@@ -44,18 +34,36 @@ export const expeciences = [
         ]
     },
     {
-        id: 4,
-        name: 'Fenalco Antioquia',
-        img: 'experience4',
-        description: 'MERN Full-Stack App where we can search for popular GitHub repositories',
-        url_web: 'https://fullstack-mern-github.onrender.com/',
+        id: 2,
+        name: 'Tera Click',
+        img: 'experience2',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur repudiandae voluptates voluptas dolorem expedita esse eum',
+        date_in: 'May 2019',
+        date_out: 'Aug 2019',
+        url_web: '#',
         tecnologies: [
-            'angular',
-            'react',
-            'vue',
-            'node',
-            'laravel',
-            'docker'
+            'html',
+            'css',
+            'php',
+            'wordpress',
+            'js',
+        ]
+    },
+    {
+        id: 1,
+        name: 'Conocimiento Competitivo',
+        img: 'experience1',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur repudiandae voluptates voluptas dolorem expedita esse eum',
+        date_in: 'Feb 2019',
+        date_out: 'May 2019',
+        url_web: 'https://conocimientocompetitivo.com/',
+        tecnologies: [
+            'android',
+            'java',
+            'c_sharp',
+            'unity',
+            'js',
+            'wordpress'
         ]
     },
 ];
--- src/pages/ExperiencePage.jsx
+++ src/pages/ExperiencePage.jsx
@@ -8,10 +8,14 @@ export const ExperiencePage = () => {
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur repudiandae voluptates voluptas dolorem expedita esse eum, et delectus impedit asperiores incidunt dolorum cumque nobis sunt? Impedit sed rerum eligendi ipsa!</p>
                 <div className="works">
                     {
-                        expeciences.map((p) =>
+                        expeciences.map((e) =>
                             <div className='experienceCard'>
-                                <img src={`/images/experiences/${p.img}.webp`} alt="Project Image" className="projectImg" width={80} height={80} />
-                                <p>{p.name}</p>
+                                <img src={`/images/experiences/${e.img}.webp`} alt="Project Image" className="projectImg" width={80} height={80} />
+                                <div className="content">
+                                    <h4>{e.name}</h4>
+                                    <p>{e.description}</p>
+                                    <small>{e.date_in} - {e.date_out}</small>
+                                </div>
                             </div>
                         )
                     }
