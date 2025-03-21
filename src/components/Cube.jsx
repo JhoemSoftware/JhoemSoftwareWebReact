8820d0f607c99b113a3ebd574fc80c684aaa37cd
Jhoem Software <www.jhonlive90@gmail.com>
2025-03-21 16:40:58 -0500

    delete backend folder

--- src/components/Cube.jsx
+++ src/components/Cube.jsx
@@ -4,7 +4,7 @@ export const Cube = () => {
     return (
         <div className="cube">
             {
-                skills.map((s) => <figure key={ s }><img src={`/images/icons/${s}.webp`} alt="Skill Image" width={200} height={200} /></figure>)
+                skills.map((s) => <figure key={ s }><img src={`/images/icons/${s}.webp`} alt="Skill Image" width={20} height={20} /></figure>)
             }
         </div>
     )
--- src/pages/PerfilPage.jsx
+++ src/pages/PerfilPage.jsx
@@ -4,9 +4,9 @@ export const PerfilPage = () => {
     return (
         <div id="PerfilPage">
             <div className="info">
-                <img src="/jhoemAzul.webp" alt="Jhoem Firm" width={250} height={250}/>
+                <img src="/jhoemAzul.webp" alt="Jhoem Firm" width={250} height={250} />
                 <p>
-                Hi! 👋🏼 I'm Jhon Alex Marín, Full Stack software developer with two years of experience in the implementation of information systems and web development. Here are my skills and some of my developed projects 👨🏻‍💻
+                    Hi! 👋🏼 I'm Jhon Alex Marín, Full Stack software developer with experience in the implementation of information systems and web development.
                 </p>
             </div>
             <Cube />
